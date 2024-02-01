dfefa70c7bd4b4351bf9de4d1dabf815e0e732e2
PlayJeri <numri.jeri@gmail.com>
2024-02-01 15:08:28 +0200

    Add additional properties to StationDetails class

--- cityBikeApp/Models/StationDetails.cs
+++ cityBikeApp/Models/StationDetails.cs
@@ -2,10 +2,14 @@
 
 namespace cityBikeApp;
 
-public partial class StationDetails
+public class StationDetails
 {
     public Station? Station { get; set; }
     public int StartedJourneysCount { get; set; }
     public int EndedJourneysCount { get; set; }
+    public int AvgJourneyDuration { get; set; }
+    public int AvgJourneyDistance { get; set; }
+    public List<TopThreeStation>? TopThreeReturnStations { get; set; }
+    public List<TopThreeStation>? TopThreeDepartureStations { get; set; }
     public List<PeakTime>? PeakTimes { get; set; }
 }
