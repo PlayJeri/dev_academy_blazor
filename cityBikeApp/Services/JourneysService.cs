89663d5bec90021c713884e34b25b34adb983c0b
PlayJeri <nurmi.jeri@gmail.com>
2024-02-07 11:22:40 +0200

    Journeys service created

---
+++ cityBikeApp/Services/JourneysService.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using cityBikeApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace cityBikeApp;
+
+public class JourneysService
+{
+    private CitybikeContext _context;
+
+    public JourneysService(CitybikeContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Journey>> GetJourneys()
+    {
+        return await _context.Journeys.Take(20).ToListAsync();
+    }
+}
