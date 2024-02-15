379fd2fbaf68edb5c028a0d8f9730163f789c9cd
PlayJeri <nurmi.jeri@gmail.com>
2024-02-15 22:10:17 +0200

    Add tests for StationService class

--- CityBikeServiceTests/StationServiceTests.cs
+++ CityBikeServiceTests/StationServiceTests.cs
@@ -8,11 +8,18 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
+/// <summary>
+/// Tests for the StationService class
+/// </summary>
 public class StationServiceTests : IDisposable
 {
     private CitybikeContext _context;
     private IStationService _stationService;
 
+    /// <summary>
+    /// Constructor for the StationServiceTests class.
+    /// Sets up a new in-memory database and a new StationService instance.
+    /// </summary>
     public StationServiceTests()
     {
         var options = new DbContextOptionsBuilder<CitybikeContext>()
@@ -23,6 +30,9 @@ public class StationServiceTests : IDisposable
         _stationService = new StationService(_context);
     }
 
+    /// <summary>
+    /// Disposes of the in-memory database after each test.
+    /// </summary>
     public void Dispose()
     {
         _context.Database.EnsureDeleted();
@@ -49,5 +59,39 @@ public class StationServiceTests : IDisposable
         // Assert
         Assert.Equal(expectedAverageDuration, averageData.Duration);
         Assert.Equal(expectedAverageDistance, averageData.Distance);
+    }
+
+    [Fact]
+    public async Task TestGetJourneysCount()
+    {
+        // Arrange
+        _context.Stations.Add(new Station { Id = 1 });
+        _context.Stations.Add(new Station { Id = 2 });
+        _context.Stations.Add(new Station { Id = 3 });
+        _context.Stations.Add(new Station { Id = 4 });
+
+        _context.Journeys.Add(new Journey { DepartureStationId = 1, ReturnStationId = 2 });
+        _context.Journeys.Add(new Journey { DepartureStationId = 2, ReturnStationId = 1 });
+        _context.Journeys.Add(new Journey { DepartureStationId = 1, ReturnStationId = 2 });
+        _context.Journeys.Add(new Journey { DepartureStationId = 1, ReturnStationId = 3 });
+
+        _context.SaveChanges();
+
+        var Station1ExpectedCounts = (startedJourneysCount: 3, endedJourneysCount: 1);
+        var Station2ExpectedCounts = (startedJourneysCount: 1, endedJourneysCount: 2);
+        var Station3ExpectedCounts = (startedJourneysCount: 0, endedJourneysCount: 1);
+        var Station4ExpectedCounts = (startedJourneysCount: 0, endedJourneysCount: 0);
+
+        // Act
+        var Station1JourneyCount = await _stationService.GetJourneysCounts(1);
+        var Station2JourneyCount = await _stationService.GetJourneysCounts(2);
+        var Station3JourneyCount = await _stationService.GetJourneysCounts(3);
+        var Station4JourneyCount = await _stationService.GetJourneysCounts(4);
+
+        // Assert
+        Assert.Equal(Station1ExpectedCounts, Station1JourneyCount);
+        Assert.Equal(Station2ExpectedCounts, Station2JourneyCount);
+        Assert.Equal(Station3ExpectedCounts, Station3JourneyCount);
+        Assert.Equal(Station4ExpectedCounts, Station4JourneyCount);
     }
 }
