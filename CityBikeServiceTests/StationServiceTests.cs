b1ad918cae2d4cedd6fc4428ba773f9be020a098
PlayJeri <nurmi.jeri@gmail.com>
2024-02-15 19:01:17 +0200

    Add test for calculating average distance and duration in StationServiceTests

--- CityBikeServiceTests/StationServiceTests.cs
+++ CityBikeServiceTests/StationServiceTests.cs
@@ -6,13 +6,48 @@ using cityBikeApp.Services;
 using cityBikeApp.Models;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-
-public class StationServiceTests
+public class StationServiceTests : IDisposable
 {
-    [Fact]
-    public void Test1()
-    {
+    private CitybikeContext _context;
+    private IStationService _stationService;
 
+    public StationServiceTests()
+    {
+        var options = new DbContextOptionsBuilder<CitybikeContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        _context = new CitybikeContext(options);
+        _stationService = new StationService(_context);
+    }
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+    }
+
+
+    [Fact]
+    public async Task TestGetAverageDistanceAndDuration()
+    {
+        // Arrange
+        _context.Journeys.Add(new Journey { DepartureStationId = 1, ReturnStationId = 2, Distance = 10, Duration = 100 });
+        _context.Journeys.Add(new Journey { DepartureStationId = 1, ReturnStationId = 2, Distance = 20, Duration = 200 });
+        _context.Journeys.Add(new Journey { DepartureStationId = 1, ReturnStationId = 2, Distance = 30, Duration = 300 });
+        _context.Journeys.Add(new Journey { DepartureStationId = 3, ReturnStationId = 4, Distance = 30, Duration = 300 });
+        _context.Journeys.Add(new Journey { DepartureStationId = 3, ReturnStationId = 5, Distance = 30, Duration = 300 });
+        _context.SaveChanges();
+
+        var expectedAverageDistance = 20;
+        var expectedAverageDuration = 200;
+
+        // Act
+        var averageData = await _stationService.GetAverageDistanceAndDuration(1);
+
+        // Assert
+        Assert.Equal(expectedAverageDuration, averageData.Duration);
+        Assert.Equal(expectedAverageDistance, averageData.Distance);
     }
 }
