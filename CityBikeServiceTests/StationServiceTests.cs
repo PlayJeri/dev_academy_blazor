3def37d72722d63907f2ee5b3ef2a48fcc1eb75c
PlayJeri <nurmi.jeri@gmail.com>
2024-02-15 18:23:24 +0200

    Add CityBikeServiceTests project and related files

---
+++ CityBikeServiceTests/StationServiceTests.cs
@@ -0,0 +1,18 @@
+namespace CityBikeServiceTests;
+
+using Xunit;
+using Moq;
+using cityBikeApp.Services;
+using cityBikeApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+public class StationServiceTests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
